6d153264183704402e70684074b53254d9f86a61
brotegor <brotegor376@gmail.com>
2023-01-27 22:37:01 +0600

    отпозиционировал карточки

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,3 +1,4 @@
+
 import { Header } from "../../components/Header";
 import background from "../../images/main.jpg";
 import s from './styles.module.scss';
@@ -6,21 +7,21 @@ export function Main() {
     return <>
         <Header />
         <main className={s.main}>
-            <ul className={s.concepts}>
-                <li className={s.concept}>
-                    <span className={s.a}>Цель</span>
-                    <span className={s.b}>Обучить</span>
-                </li>
-                <li className={s.concept}>
-                    <span className={s.a}>Важность</span>
-                    <span className={s.b}>Очень важно</span>
-                </li>
-                <li className={s.concept}>
-                    <span className={s.a}>Уникальность</span>
-                    <span className={s.b}>Самый уникальный</span>
-                </li>
-            </ul>
-            <img src={background} alt="wergwgesdewg" className={s.mainimage}/>
+            <div className={s.concepts}>
+                <div className={`${s.concept} ${s.leftConcept}`}>
+                    <h3 className={s.defaultText}>Цель</h3>
+                    <p className={s.hoverText}>Обучить</p>
+                </div>
+                <div className={`${s.concept} ${s.middleConcept}`}>
+                    <h3 className={s.defaultText}>Важность</h3>
+                    <p className={s.hoverText}>Очень важно</p>
+                </div>
+                <div className={`${s.concept} ${s.rightConcept}`}>
+                    <h3 className={s.defaultText}>Уникальность</h3>
+                    <p className={s.hoverText}>Самый уникальный</p>
+                </div>
+            </div>
+            <img src={background} alt="background image" className={s.mainimage}/>
         </main>  
     </> 
 }

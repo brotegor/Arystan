48da53a1fe2bb066c8b25ec3da1e159265d8b608
abendyukov <arseniy.bendyukov2006@gmail.com>
2023-02-04 19:27:06 +0600

    /profile: рефакторинг страницы

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,64 +1,25 @@
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import s from './styles.module.scss';
-import profile from '../../images/profile.svg';
-import settings from '../../images/settings.svg';
+import { repeat } from '../../utils';
+import { AccountCard } from "./AccountCard";
+import { ScrollContainer } from "./ScrollContainer";
 
 export function Profile() {
     return (
         <>
             <Header />
-            <Container className={s.Container}>
-                <div className={s.mineCourse}>
-                    <div className={s.achievements}>
-                        Мои курсы
-                    </div>
-                    <div className={s.mineCoursesCase}>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        <div className={s.infoBlock}></div>
-                        
-                    </div>
-                </div>
-                <div className={s.AccountContainer}>
-                    <div className={s.HalfAccount}>
-                        <div className={s.microContainer}>
-                           <img src={profile} alt="profile" className={s.icon}/>
-                           <h1 className={s.Heading}>Bubilda</h1>
-                           <img src={settings} alt="settings" className={s.settings}/>
-                        </div>
-                        <div className={s.levelContainer}>
-                            <div className={s.level}>
-                                Level: 0
-                            </div>
-                            <div className={s.progressValue}>
-                                70/100
-                            </div>        
-                        </div>
-                        <div className={s.progressBar}>
-                            <div className={s.progress}></div>
-                        </div>
-                    </div>
-
-                    <div className={s.HalfAccount}>
-                        <div className={s.achievements}>Достижения</div>
-                        <div className={s.mineAchievementsCase}>
-                        <div className={s.infoBlock}>
-                            <div className={s.infoBlock}></div>
-                            <div className={s.infoBlock}></div>
-                            <div className={s.infoBlock}></div>
-                            <div className={s.infoBlock}></div>
-                            <div className={s.infoBlock}></div>
-                            <div className={s.infoBlock}></div>
-                        </div>
-                        </div>
-                    </div>
+            <Container className={s.container}>
+                <ScrollContainer height="calc(100vh - 270px)" className={s.courses} heading="Мои курсы">
+                    {repeat(() => <div className={s.skeleton}></div>, 10)}
+                </ScrollContainer>
+                <div className={s.accountContainer}>
+                    <AccountCard />
+                    <ScrollContainer height="calc(100vh - 523px)" className={s.achievements} heading="Достижения">
+                        {repeat(() => <div className={s.skeleton}></div>, 4)}
+                    </ScrollContainer>
                 </div>
             </Container>
         </>
     );
-  }
+}

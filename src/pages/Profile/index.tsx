8b89a2b3a4237530e8fcb555ac91334847c83027
abendyukov <arseniy.bendyukov2006@gmail.com>
2023-02-07 15:07:48 +0600

    /profile: начал перевёрстывать профиль

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,23 +1,24 @@
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import s from './styles.module.scss';
-import { repeat } from '../../utils';
-import { AccountCard } from "./AccountCard";
-import { ScrollContainer } from "./ScrollContainer";
+import { MainLink } from '../../components/MainLink';
 
 export function Profile() {
     return (
         <>
             <Header />
             <Container className={s.container}>
-                <ScrollContainer height="calc(100vh - 270px)" className={s.courses} heading="Мои курсы">
-                    {repeat(() => <div className={s.skeleton}></div>, 10)}
-                </ScrollContainer>
-                <div className={s.accountContainer}>
-                    <AccountCard />
-                    <ScrollContainer height="calc(100vh - 523px)" className={s.achievements} heading="Достижения">
-                        {repeat(() => <div className={s.skeleton}></div>, 4)}
-                    </ScrollContainer>
+                <div className={s.profileWrapper}>
+                    <img className={s.profileImage} src="" alt="" />
+                    <MainLink className={s.button} source='#' title='Настройки'/>
+                    <div className={s.banner}></div>
+                </div>
+                <div className={s.informationWrapper}>
+                    <div className={s.achivements}>
+                        <p className={s.achivementsHeading}>Достижения</p>
+                    </div><div className={s.activity}>
+                        <p className={s.activityHeading}>Активность</p>
+                    </div>
                 </div>
             </Container>
         </>
